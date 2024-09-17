3e1f5c08b4c588911baf7e8a7a108a356ee13c9b
LemoZun <90059335+LemoZun@users.noreply.github.com>
2024-09-17 15:10:31 +0900

    feat : 발사 딜레이 구현

--- SpaceWatermelon/Assets/Scripts/PlanetLauncher.cs
+++ SpaceWatermelon/Assets/Scripts/PlanetLauncher.cs
@@ -8,22 +8,38 @@ public class PlanetLauncher : MonoBehaviour
     [SerializeField] float rotateSpeed;
     [SerializeField] GameObject planetPrefab;
     [SerializeField] Transform launchPosition;
+    [SerializeField] GameObject launchChecker;
+    Coroutine launchRoutine;
+    bool launchPossible = true;
 
     private void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        
+        if(launchRoutine == null)
+            launchRoutine = StartCoroutine(LaunchRoutine());
+        launchChecker.SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        if(launchRoutine != null)
+            StopCoroutine(launchRoutine);
     }
     private void Update()
     {
         float x = Input.GetAxis("Mouse X");
         transform.Rotate(0,0,-x*rotateSpeed*Time.deltaTime);
 
-
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && launchPossible)
         {
             LaunchPlanet();
         }
+        //if(Input.GetMouseButtonDown(0))
+        //{
+        //    LaunchPlanet();
+        //}
     }
 
     private void LaunchPlanet()
@@ -32,5 +48,28 @@ public class PlanetLauncher : MonoBehaviour
         GameObject planet = Instantiate(planetPrefab, launchPosition.position,Quaternion.identity);
         Rigidbody2D rb = planet.GetComponent<Rigidbody2D>();
         rb.AddForce(dir * launchFore, ForceMode2D.Impulse);
+        launchPossible = false;
+        launchChecker.SetActive(false);
+        Debug.Log("Charging");
+    }
+
+    IEnumerator LaunchRoutine()
+    {
+        WaitForSeconds delay = new WaitForSeconds(2f);
+
+        while (true)
+        {
+            yield return delay;
+
+            if(!launchPossible)
+            {
+                launchPossible = true;
+                launchChecker.SetActive(true);
+                Debug.Log("Charging Complete");
+            }
+        }
+
+
+
     }
 }
